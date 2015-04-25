ce32f77cbd9b53a308e5fc8ed01c26f7a43be462
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-25 10:19:29 +0530

    Complete about_open_classes; about_inheritance; about_dice_project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_accessor :values
+
+  def roll(n)
+    @values = []
+    n.times {@values<<rand(1..6)}
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -14,7 +20,6 @@ class AboutDiceProject < Neo::Koan
 
   def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
     dice = DiceSet.new
-
     dice.roll(5)
     assert dice.values.is_a?(Array), "should be an array"
     assert_equal 5, dice.values.size
