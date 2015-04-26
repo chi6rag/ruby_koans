25a1e4065f3a4b026607e5af45fc37d71f4f7af6
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-26 12:11:08 +0530

    Add about proxy object project

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,13 +13,35 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  
+  attr_reader :messages
+  
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to? method_name then
+      @messages.push method_name
+      @object.send method_name, *args
+    else
+      super method_name, *args, &block
+    end
+  end
+
+  def called?(sym)
+    @messages.include? sym
+  end
+
+  def number_of_times_called sym
+    @messages.count sym 
+  end
+
 end
+
+# proxy_one = Proxy.new(Television.new)
+# puts proxy_one.channel
 
 # The proxy object should pass the following Koan:
 #
